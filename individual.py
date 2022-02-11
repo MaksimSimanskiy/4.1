9f0fd4123bdca61cfd246c0871e3efecef99c18c
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2022-02-11 11:26:50 +0300

    Add files via upload

---
+++ individual.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import random
+
+"""
+Поле first — целое число, целая часть числа; поле second — положительное целое число,
+дробная часть числа. Реализовать метод multiply() — умножение на произвольное целое
+число типа int. Метод должен правильно работать при любых допустимых значениях first и
+second.
+"""
+
+class Task:
+
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+        if (self.first <= 0) or (self.second <= 0):
+            raise ValueError()
+
+
+    def read(self):
+        self.first = input("Введите целую часть числа ")
+        self.second = input("Введите дробную часть числа ")
+
+
+    def display(self):
+        print(f"Число с плавающей точкой {self.first}.{self.second}")
+
+
+    def multiply(self):
+        randn = random.randint(1, 100)
+        fln = float(str(self.first) + "." + str(self.second))
+        c = fln * randn
+        print("Случайное число - ", randn)
+        print("Результат умножения - ", c)
+
+
+if __name__ == '__main__':
+    newTask = Task(12, 5)
+    newTask.read()
+    newTask.display()
+    newTask.multiply()
